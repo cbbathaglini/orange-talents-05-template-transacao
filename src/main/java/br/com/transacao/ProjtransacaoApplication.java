ca7c63a3cb94dbb7afc23102a7ec36e6d0a162c5
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2021-06-11 09:42:41 -0300

    00.setup-projeto

---
+++ src/main/java/br/com/transacao/ProjtransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjtransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjtransacaoApplication.class, args);
+	}
+
+}
